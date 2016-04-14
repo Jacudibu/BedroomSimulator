7f3e3b1334a513574960dc55c70cea6f87427420
Robert <robert.brand@tum.de>
2016-04-14 19:43:42 +0200

    added randomly spawning mouses
    their rotation is currently still fucked up though

--- Assets/Scripts/Child.cs
+++ Assets/Scripts/Child.cs
@@ -10,6 +10,10 @@ public class Child : NetworkObject
     int totalSocks = 20;
     public static int currentSocks = 20;
 
+    //mouse spawning
+    public GameObject mousePrefab;
+    public Rect spawnArea;
+
 	override public void Start ()
     {
         base.Start();
@@ -79,6 +83,8 @@ public class Child : NetworkObject
         GetComponent<AudioSource>().loop = false;
 
         StartCoroutine(SetFlashlight(true, 1.5f));
+
+        StartCoroutine(SpawnMice());//start spawning mice at intervals
     }
 
     IEnumerator SetFlashlight(bool status, float delay)
@@ -91,5 +97,94 @@ public class Child : NetworkObject
         {
             light.enabled = status;
         }
+    }
+
+    IEnumerator SpawnMice()
+    {
+        yield return new WaitForSeconds(10.0f);
+
+        GameObject spawnedMouse = null;
+
+        while (hasAuthority)
+        {
+            float nextDelay = Random.Range(30.0f, 60.0f);
+
+            if (spawnedMouse != null)
+            {
+                //delete the mouse
+                NetworkServer.UnSpawn(spawnedMouse);
+                GameObject.Destroy(spawnedMouse);
+            }
+
+            spawnedMouse = spawnMouse();
+
+            yield return new WaitForSeconds(nextDelay);
+        }
+    }
+
+    private GameObject spawnMouse()
+    {
+        if (!hasAuthority) return null;
+
+        int side = Random.Range(0, 4);//which side of the room to spawn the mouse at
+        float length = Random.value;//how far along the length of that side to spawn
+
+        Vector2 spawnPos = Vector2.zero;
+        Vector2 dir = Vector2.zero;
+        float z_angle = 0;
+
+        switch (side)
+        {
+            //left
+            case 0:
+                {
+                    spawnPos.x = spawnArea.xMin;
+                    spawnPos.y = spawnArea.height * length;
+
+                    dir = new Vector2(1, 0);
+                    z_angle = 90;
+                    break;
+                }
+            //right
+            case 1:
+                {
+                    spawnPos.x = spawnArea.xMax;
+                    spawnPos.y = spawnArea.height * length;
+
+                    dir = new Vector2(-1, 0);
+                    z_angle = 270;
+                    break;
+                }
+            //top
+            case 2:
+                {
+                    spawnPos.x = spawnArea.width * length;
+                    spawnPos.y = spawnArea.yMax;
+
+                    dir = new Vector2(0, -1);
+                    z_angle = 180;
+                    break;
+                }
+            //bottom
+            case 3:
+                {
+                    spawnPos.x = spawnArea.width * length;
+                    spawnPos.y = spawnArea.yMin;
+
+                    dir = new Vector2(0, 1);
+                    z_angle = 0;
+                    break;
+                }
+        }
+
+
+        //actually spawn
+        GameObject mouse = (GameObject)GameObject.Instantiate(mousePrefab, new Vector3(spawnPos.x, 0, spawnPos.y), Quaternion.identity);
+        mouse.GetComponent<Mouse>().direction = dir;
+        mouse.transform.eulerAngles = new Vector3(mouse.transform.eulerAngles.x, mouse.transform.eulerAngles.y, z_angle);
+
+        NetworkServer.Spawn(mouse);
+
+        return mouse;
     }
 }
---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Mouse : MonoBehaviour
+{
+    //movement parameters
+    public float speed;
+    public Vector2 direction;
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        //start making noises
+        StartCoroutine("NoiseCoroutine");
+	}
+
+    void Update()
+    {
+        //movement
+        transform.Translate(speed * new Vector3(direction.x, 0, direction.y) * Time.deltaTime, Space.World);
+	}
+
+    IEnumerator NoiseCoroutine()
+    {
+        //makes a noise at randomized intervals
+        while (true)
+        {
+            float nextDelay = Random.Range(1.5f, 3.5f);
+
+            audioSource.Play();
+
+            yield return new WaitForSeconds(nextDelay);
+        }
+    }
+}
